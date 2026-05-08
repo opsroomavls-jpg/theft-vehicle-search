b45253e1bdb2ab9e54c821ff3c1930057bb63fa9
opsroomavls-jpg <opsroomavls@gmail.com>
2026-05-08 22:43:06 +0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+var CACHE_NAME = "vehicle-search-v1";
+var urlsToCache = ["./index.html"];
+
+self.addEventListener("install", function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        }).catch(function() {
+            return caches.match("./index.html");
+        })
+    );
+});
